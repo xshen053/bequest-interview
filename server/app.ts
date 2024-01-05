33507adc839b0c8f29304649a74e13754ece5516
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-01-05 14:43:06 -0800

    finished the problem

--- server/app.ts
+++ server/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const database = {token: "client1" , data: "Hello World" , serverSeqNum: 0};
 
 app.use(cors());
 app.use(express.json());
@@ -14,9 +14,19 @@ app.get("/", (req, res) => {
   res.json(database);
 });
 
+/**
+ * Only when @token match address of client will server update the data.
+ * Otherwise, ignore
+ */
 app.post("/", (req, res) => {
-  database.data = req.body.data;
-  res.sendStatus(200);
+  if (req.body.address == database.token) {
+    database.serverSeqNum += 1
+    database.data = req.body.data;
+    // console.log(database)
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 app.listen(PORT, () => {
